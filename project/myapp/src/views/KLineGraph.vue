a468f522fe4fb5b4b6b892acb4cf95127ecfea7e
丁佳男 <jiananst@qq.com>
2022-08-18 02:12:28 +0800

    学到雷达图了

--- project/myapp/src/views/KLineGraph.vue
+++ project/myapp/src/views/KLineGraph.vue
@@ -6,6 +6,21 @@
 import * as echarts from "echarts";
 
 export default {
+  data() {
+    return {
+      kData: [
+        [20, 34, 10, 38],
+        [40, 35, 30, 50],
+        [31, 38, 33, 44],
+        [38, 15, 5, 42],
+      ]
+    }
+  },
+  computed: {
+    newData() {
+      return this.kData.map(v => v[0])
+    }
+  },
   mounted() {
     let myEcharts = echarts.init(this.$refs.myChart);
     let option = {
@@ -23,20 +38,40 @@ export default {
       series: [
         {
           type: "candlestick", //K线图
-          data: [
-            [20, 34, 10, 38],
-            [40, 35, 30, 50],
-            [31, 38, 33, 44],
-            [38, 15, 5, 42],
-          ],
+          data: this.kData,
           //设置颜色
           itemStyle: {
             color: '#ec5566', //上涨颜色
-            color0: '#00da3c', //下跌颜色
+            color0: '#55da3c', //下跌颜色
             borderColor: '#8A0000', //上涨的边框色
             borderColor0: '#008f28', //下跌的边框色
+          },
+          //设置最值
+          markPoint: {
+            data: [
+              {
+                name: '最大值',
+                type: 'max',
+                valueDim: 'highest', //在哪个维度上设置最值
+              },
+              {
+                name: '最小值',
+                type: 'min',
+                valueDim: 'lowest'
+              },
+              {
+                name: '平均值',
+                type: 'average',
+                valueDim: 'close'
+              }
+            ]
           }
         },
+        {
+          type: 'line',
+          smooth: true,
+          data: this.newData
+        }
       ],
     };
     myEcharts.setOption(option);
